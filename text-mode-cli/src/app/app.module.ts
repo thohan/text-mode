45336007e40810e7f60817ba731717e6c803af5f
Bryan <bbean11@outlook.com>
2018-02-10 22:16:20 -0700

    For now, the specs will just be in with the components.

--- text-mode-cli/src/app/app.module.ts
+++ text-mode-cli/src/app/app.module.ts
@@ -8,7 +8,8 @@ import { DalekComponent } from './daleks/dalek.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DalekComponent
   ],
   imports: [
     BrowserModule,
--- text-mode-cli/src/app/daleks/dalek.component.ts
+++ text-mode-cli/src/app/daleks/dalek.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'daleks',
-    templateUrl: './dalek-component.html'
+    templateUrl: './dalek.component.html'
 })
 
 export class DalekComponent{
