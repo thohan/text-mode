de8b1924a8d8370ed5f25f4e65b1d25c7b1f2085
Bryan <bbean11@outlook.com>
2018-02-15 21:01:20 -0700

    directory renaming, some spacing/formatting

--- text-mode-cli/src/app/daleks/dalek.component.ts
+++ text-mode-cli/src/app/daleks/dalek.component.ts
@@ -3,45 +3,45 @@ import { DecimalPipe } from '@angular/common';
 import * as DalekModel from './dalek.model';
 
 @Component({
-    selector: 'daleks',
-    templateUrl: './dalek.component.html'
+	selector: 'daleks',
+	templateUrl: './dalek.component.html'
 })
 
 export class DalekComponent implements OnInit, AfterViewInit {
-    cursor: DalekModel.Cursor;
-    ctx: CanvasRenderingContext2D;
-    doctor: DalekModel.Doctor;
+	cursor: DalekModel.Cursor;
+	ctx: CanvasRenderingContext2D; 
+	doctor: DalekModel.Doctor;
 
-    @ViewChild('canvas') canvas: ElementRef;
-    
-    showMousePosition(evt) {
-        var rect = this.canvas.nativeElement.getBoundingClientRect();
-        this.cursor.xpos = evt.clientX - rect.left;
-        this.cursor.ypos = evt.clientY - rect.top;
-    }
+	@ViewChild('canvas') canvas: ElementRef;
 
-    ngOnInit() {
-        this.ctx = this.canvas.nativeElement.getContext('2d');
+	showMousePosition(evt) {
+		var rect = this.canvas.nativeElement.getBoundingClientRect();
+		this.cursor.xpos = evt.clientX - rect.left;
+		this.cursor.ypos = evt.clientY - rect.top;
+	}
 
-        this.cursor = new DalekModel.Cursor();
-        this.cursor.xpos = 0;
-        this.cursor.ypos = 0;
-        
-        // Now I can draw stuff! (I just need to remember how...)
-        this.doctor = new DalekModel.Doctor();
-        this.doctor.xpos = 280;
-        this.doctor.ypos = 200
+	ngOnInit() {
+		this.ctx = this.canvas.nativeElement.getContext('2d');
+
+		this.cursor = new DalekModel.Cursor();
+		this.cursor.xpos = 0;
+		this.cursor.ypos = 0;
+
+		// Now I can draw stuff! (I just need to remember how...)
+		this.doctor = new DalekModel.Doctor();
+		this.doctor.xpos = 280;
+		this.doctor.ypos = 200;
 
 
-        // draw the doctor on the canvas:
-        this.doctor.image = <HTMLImageElement>document.getElementById('doctor');
-        this.doctor.image.onload = ()=>{
-            this.ctx.drawImage(this.doctor.image, 50, 50);
-        };
-    };
+		// draw the doctor on the canvas - Image assets loaded on the dom for use by the canvas. See http://www.typescriptgames.com/ImageToCanvas.html for reference.
+		this.doctor.image = <HTMLImageElement>document.getElementById('doctor');
+		this.doctor.image.onload = () => {
+			this.ctx.drawImage(this.doctor.image, this.doctor.xpos, this.doctor.ypos);
+		};
+	};
 
-    ngAfterViewInit(){
+	ngAfterViewInit() {
 
-        
-    }
+
+	}
 }
--- text-mode.web/App_Start/BundleConfig.cs
+++ text-mode.web/App_Start/BundleConfig.cs
@@ -9,16 +9,16 @@ namespace text_mode.web
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+						"~/src/jquery-{version}.js"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
+						"~/src/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js",
-					  "~/Scripts/respond.js"));
+					  "~/src/bootstrap.js",
+					  "~/src/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
